865b4e30e586cbefc40ad8acc5d3a57cac122cd9
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2020-11-20 12:24:52 +0530

    Site update

---
+++ pages/GuidePage.jsx
@@ -0,0 +1,127 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Header from '../components/Header';
+import Markdown from 'react-showdown';
+
+let sidebarOpen = true;
+if(innerWidth > 700) sidebarOpen = false;
+
+const sidebar = () => {
+    if(!sidebarOpen){
+        sidebarOpen = true;
+        document.getElementById('doc-section').style.width = (innerWidth - 250) + 'px';
+        document.getElementById('doc-sidebar').style.display = 'block';
+    }else{
+        sidebarOpen = false;
+        document.getElementById('doc-sidebar').style.display = 'none'
+        document.getElementById('doc-section').style.width = '100%'
+    };
+};
+
+const makeSidebarLinks = () => {
+    let links = [];
+    document.querySelectorAll('#doc-section h1, #doc-section h2').forEach(x => {
+        links.push(
+            x.tagName === 'H1' ?
+            `<a class="large-sidebar-link" onclick="document.getElementById('${x.innerHTML.toLowerCase().replace(/[|&;$%@"<>()+,\-. ]/g, '')}').scrollIntoView()">${x.innerHTML}</a>` :
+            `<a class="small-sidebar-link" onclick="document.getElementById('${x.innerHTML.toLowerCase().replace(/[|&;$%@"<>()+,\-. ]/g, '')}').scrollIntoView()">${x.innerHTML}</a>`
+        );
+    });
+
+    document.getElementById('sub-sidebar').innerHTML = `<div class="sidebar-links">${links.join('<br/>')}</div>`;
+};
+class GuidePage extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = { content: 'Loading guide...' };
+        this.guide = { updated: '404' };
+        this.content = [];
+    };
+
+    componentWillMount(){
+        let queries = new URLSearchParams(location.hash.split('?')[1]);
+        let page = queries.get('page');
+        let guide = this.props.data.find(x => x.file === this.props.guide);
+        if(!guide) return this.setState({ content: '404 - Not Found' });
+        this.guide = guide;
+
+        fetch(`https://raw.githubusercontent.com/decimaldevteam/guides/main/public/guides/${guide.file}.md`)
+        .then(res => res.text())
+        .then(content => {
+            this.content = content.split('---');
+            let i = page ? parseInt(page) : 0;
+            this.setState({ content: this.content[i] });
+            window.guidepage = this;
+            window.guidecontent = this.content;
+
+            window.highlight();
+            sidebar();
+            window.addEventListener('resize', sidebar);
+
+            let links = [];
+            let paginator = [];
+            for(let i = 0; i < guide.sidebar.length; i++) {
+                links.push(`<a class="large-sidebar-link" onclick="window.guidepage.setState({ content: window.guidecontent[${i}] })">${guide.sidebar[i]}</a>`);
+                paginator.push(`<a class="paginator-link" onclick="window.guidepage.setState({ content: window.guidecontent[${i}] })">${i+1}</a>`);
+            };
+            document.getElementById('sidebar-link-handler').innerHTML = `<div class="sidebar-links"><h2>Pages</h2><br/>${links.join('<br/>')}</div>`;
+            document.getElementById('paginator').innerHTML = paginator.join('\n');
+            document.getElementById('writter').innerHTML = guide.contributors
+            .map(x => `<a href="http://github.com/${x}">${x}</a>`)
+            .join(', ')
+
+            let oldHistory = localStorage.getItem('history');
+            if(!oldHistory) return localStorage.setItem('history', guide.file);
+            localStorage.setItem('history', `${oldHistory},${guide.file}`);
+        });
+    };
+
+    componentDidUpdate(){
+        makeSidebarLinks();
+        window.highlight();
+    };
+
+    componentWillUnmount(){
+        window.removeEventListener('resize', sidebar);
+    };
+
+    componentDidMount(){
+        window.darkMode = true;
+        window.dark();
+    };
+
+    render(){
+        return <>
+            <Header/>
+            
+            <div id="doc-sidebar">
+                <div id="sub-sidebar"></div>
+                <div id="sidebar-link-handler"></div>
+            </div>
+
+            <div className="padding-bottom display-area guide-page" id="doc-section">
+                <Markdown
+                    markdown={this.state.content}
+                    options={{ tables: true }}
+                />
+                <div id="paginator"/>
+                <hr/>
+                <font className="credits">Written by <font id="writter">404</font></font><br/>
+                <font className="muted">Last updated at {this.guide.updated}</font><br/>
+                <a className="edit-btn" href={`https://github.com/decimaldevteam/guides/blob/main/public/guides/${this.guide.file}.md`}>Edit this guide <i class="fas fa-edit"></i></a>
+                <div style={{ height: '90px' }}/>
+            </div>
+
+            <div id="nav-btn">
+                <a onClick={sidebar}><i className="fa fa-bars fa-2x" aria-hidden="true"></i></a>
+            </div>
+        </>
+    };
+
+};
+
+export default GuidePage;
---
+++ pages/Home.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import Header from '../components/Header';
+import GuideCard from '../components/GuideCard';
+
+const match = (a, b) => {
+    for(let i = 0; i < a.length; i++) if(b.includes(a[i])) return true;
+};
+
+class Home extends React.Component{
+
+    componentDidMount(){
+        window.darkMode = true;
+        window.dark();
+    };
+
+    render(){
+        let data = this.props.data;
+        let history = localStorage.getItem('history');
+        let topGuides = data.filter(x => x.top);
+
+        const LatestGuides = () => data.slice(0, 4).map(x => <GuideCard guide={x}/>);
+        const TopGuides = () => topGuides.map(x => <GuideCard guide={x}/>);
+        const RandomGuides = () => data.sort(() => Math.random() - 0.5).slice(0, 4).map(x => <GuideCard guide={x}/>);
+        const RecentlyViewedGuides = () => data.filter(x => history.includes(x.file)).slice(0, 4).map(x => <GuideCard guide={x}/>);
+        const RecommendedGuides = () => topGuides.filter(x => match(x.tags, history)).slice(0, 4).map(x => <GuideCard guide={x}/>);
+
+        return <>
+            <Header/>
+
+            <div className="coverpage">
+                <div className="coverpage-content">
+                    <h2>Decimal Guides</h2>
+                    <p>Guides for developers by developers!</p>
+                    <a href="/#/guides">Get Started</a>
+                </div>
+            </div>
+
+            <div className="display-area">
+                <h3 className="title">Latest Guides</h3>
+                <div className="row" style={{ marginTop: '10px' }}>
+                    <LatestGuides/>
+                </div>
+
+                <h3 className="title">Recently Viewed Guides</h3>
+                <div className="row" style={{ marginTop: '10px' }}>
+                    <RecentlyViewedGuides/>
+                </div>
+
+                <h3 className="title">Recommended Guides</h3>
+                <font>This is based on your history...</font>
+                <div className="row" style={{ marginTop: '10px' }}>
+                    <RecommendedGuides/>
+                </div>
+
+                <h3 className="title">Top Guides</h3>
+                <div className="row" style={{ marginTop: '10px' }}>
+                    <TopGuides/>
+                </div>
+
+                <h3 className="title">Random Guides</h3>
+                <div className="row" style={{ marginTop: '10px' }}>
+                    <RandomGuides/>
+                </div>
+
+                <div className="footer-info-text">
+                    <a href="/#/guides"><font>Explore more guides <i class="fas fa-arrow-right"></i></font></a>
+                </div>
+
+                <div style={{ height: '60px' }}/>
+            </div>
+        </>
+    };
+
+};
+
+export default Home;
