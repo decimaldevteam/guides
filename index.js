7f26d989060008f014d8960d194abb4fb581eeec
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2021-01-15 11:22:13 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const next = require('next')({ dev: process.argv[2] == 'dev' });
+const handle = next.getRequestHandler();
+
+next.prepare().then(() => {
+
+    app.use('/api', require('./api'));
+
+    app.get('*', (req, res) => {
+        handle(req, res);
+    })
+
+    app.listen(3000, () => console.log(`[EVENT] - Web server is ready!`));
+    
+})
